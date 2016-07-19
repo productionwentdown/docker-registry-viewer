fcdc959f6259171a8d72080c2b88a2179906d141
mkdym <abju123@sina.com>
2016-07-19 21:19:44 +0800

    volume bin to tmp

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ set -e
 cd `dirname $0`
 
 
+sudo rm -rf /tmp/docker-registry-viewer/bin
 docker run -ti --rm \
-    -v `pwd`:/go/src/github.com/mkdym/docker-registry-viewer \
-    -v `pwd`/bin:/go/bin mkdym/golang:alpine-git \
+    -v `pwd`:/go/src/github.com/mkdym/docker-registry-viewer:ro \
+    -v /tmp/docker-registry-viewer/bin:/go/bin mkdym/golang:alpine-git \
     /bin/sh -c "go get -v github.com/gin-gonic/gin && go install github.com/mkdym/docker-registry-viewer"
+
+sudo rm -rf ./bin
+sudo mv /tmp/docker-registry-viewer/bin/ ./bin
 docker build -t docker-registry-viewer:v1.0 .
