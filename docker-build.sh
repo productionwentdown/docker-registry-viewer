3eb219552fb75f4794c83aa1f4b872fab9503381
juxiaoheng <juxiaoheng@kingsoft.com>
2016-07-20 12:10:30 +0800

    add use gopm

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
+set -x
 set -e
 cd `dirname $0`
+
+
+COMMAND_GET="go get -v github.com/gin-gonic/gin"
+COMMAND_GET_GOPM="gopm get -v -g github.com/gin-gonic/gin"
+COMMAND_BUILD="go build -o /docker-bin/docker-registry-viewer github.com/mkdym/docker-registry-viewer"
+
+COMMAND="${COMMAND_GET} && ${COMMAND_BUILD}"
 
 
 sudo rm -rf /tmp/docker-registry-viewer/docker-bin
 docker run -ti --rm \
     -v `pwd`:/go/src/github.com/mkdym/docker-registry-viewer:ro \
     -v /tmp/docker-registry-viewer/docker-bin:/docker-bin mkdym/golang:alpine-git \
-    /bin/sh -c "go get -v github.com/gin-gonic/gin && go build -o /docker-bin/docker-registry-viewer github.com/mkdym/docker-registry-viewer"
+    /bin/sh -c "$COMMAND"
 
 sudo rm -rf ./docker-bin
 sudo mv /tmp/docker-registry-viewer/docker-bin/ ./docker-bin
